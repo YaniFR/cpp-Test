87609bf5a456232889908056f0826b1ca59a3964
YaniFR <yanifrkherzane@gmail.com>
2023-11-15 17:51:23 +0100

    kamaboko

--- main.cpp
+++ main.cpp
@@ -2,15 +2,15 @@
 #include <vector>
 using namespace std;
 
-vector<string> get_strings(string caca1){
+vector<char *> get_strings(char *caca1){
 
     // création d'un tableau
-    vector< string > liste_de_caca; 
+    vector<char *> liste_de_caca; 
     
     // réservation de 2 places maximum pour le tableau
     liste_de_caca.reserve(2); 
 
-    string caca = "mortier au caca";
+    char caca[16] = "mortier au caca";
 
     // equivalent de append
     liste_de_caca.push_back(caca); 
@@ -50,15 +50,16 @@ string leplusgros(){
     return legros;
 }
 
-int main(int argc,string argv ){
+int main(int argc,char **argv){
 
     short unsigned int pos {8}; // 2 bytes, valeur seulement positif grace au unsigned
     auto pos2 {8u}; // determine automatiquement unsigned int 
     short signed int neg {-12}; // 2 bytes, valeur acceptant les nombres négatifs grace au signed
 
-    vector<string> texte = get_strings(argv);
-    cout << texte[0] << pos << endl;        
-    cout << texte[1] << neg << endl;
+    vector<char *> texte = get_strings(argv[1]);
+    cout << texte[0] << endl << pos << endl;        
+    cout << texte[1] << endl << neg << endl;
+    cout << argc << endl;
     cout << texte.size() << std::endl;
     
     return 0;
